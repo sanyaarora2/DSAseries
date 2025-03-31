010f3f256d6f92650f44dc0c22e27c46f4d052a3
sanyaarora2 <sanyaa2711@gmail.com>
2025-03-31 20:10:11 +0530

    day28.cpp

--- day28.cpp
+++ day28.cpp
@@ -20,21 +20,17 @@ public:
 Ques 2
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        
-        
+    ListNode* middleNode(ListNode* head) {
         ListNode* slow=head;
         ListNode* fast=head;
         while(fast!=NULL && fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast)
-            return true;
+
         }
-        return false;
+        return slow;
 
         
-    }
-};
+    
 TC= O(n) for both
 SC=O(1) for both
